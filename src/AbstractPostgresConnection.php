a51d54d7103accb592ac9fbfcf2049f0dde72646
charlie <charlie@charlie-OptiPlex-GX270>
2015-06-17 00:03:13 -0400

    More connection types

--- src/AbstractPostgresConnection.php
+++ src/AbstractPostgresConnection.php
@@ -13,27 +13,27 @@ namespace GCG\Core;
  *
  * @author charlie
  */
-abstract class AbstractMysqlConnection {
+abstract class AbstractPostgresConnection {
     // Meedo database connection
     protected $conx = [];
-            
+    
     /**
      * Gets an existing or creates a new stored connection to return
      * 
      * @param type $source
-     * @return MySQL
+     * @return Postgres
      */
-    public function getMysqlConnection($source) {
+    public function getPostgresConnection($source) {
         if(($this->conx === null)?true:!array_key_exists($source, $this->conx)) {
-            $this->conx[$source] = $this->createMysqlConnection($source);
+            $this->conx[$source] = $this->createPostgresConnection($source);
         }
         return $this->conx[$source];
     }
     
-    public function createMysqlConnection($source) {
+    public function createPostgresConnection($source) {
         //Access configuration values from default location (/usr/local/etc/gpg/default)
         $config = new \Configula\Config('/usr/local/etc/gpg/default');
         
-        return new \medoo($config->mysqlConfig[$source]); // medoo does not use namespaces
+        return new \medoo($config->postgresConfig[$source]); // medoo does not use namespaces
     }
 }
